6bcddacb40f32e61bf0d7f9d2acb83afa721cdfb
Edsel <edsel@aviatrix.com>
2018-03-29 17:03:58 -0700

    terraform gateway config

---
+++ gateway.tf
@@ -0,0 +1,17 @@
+# Create Aviatrix gateway in AWS public cloud 
+#
+# To Enable HA Gateway add line  "ha_subnet" keyword 
+# To Disable HA Gateway delete "ha_subnet" keyword 
+
+resource "aviatrix_gateway" "aws_gateway" {
+     cloud_type = "${var.aviatrix_cloud_type_aws}"
+   account_name = "${var.aviatrix_cloud_account_name}"
+        gw_name = "${var.aviatrix_gateway_name}"
+         vpc_id = "${var.aws_vpc_id}"
+        vpc_reg = "${var.aws_region}"
+       vpc_size = "${var.aws_instance}"
+        vpc_net = "${var.aws_vpc_public_cidr}"
+      ha_subnet = "${var.aviatrix_gateway_ha_subnet}"
+}
+      #ha_subnet = "${var.aviatrix_gateway_ha_subnet}"
+
---
+++ terraform.tfvars
@@ -0,0 +1,17 @@
+                  aws_region = "us-east-1"
+                  aws_vpc_id = "vpc-5b410323"
+                aws_instance = "t2.micro"
+         aws_vpc_public_cidr = "10.10.0.0/24"
+
+      aviatrix_controller_ip = "52.52.207.238"
+aviatrix_controller_username = "admin"
+aviatrix_controller_password = "Aviatrix123!"
+aviatrix_cloud_account_name  = "habackup"
+      aviatrix_gateway_name  = "mytest-aviatrix-gateway"
+     aviatrix_cloud_type_aws = 1
+  aviatrix_gateway_ha_subnet = "10.10.0.0/24"
+
+## Enable HA Gateway
+#  aviatrix_gateway_ha_subnet = "10.10.0.0/24"
+
+
